3c8affc41fcc559cd6ee8d292840bd7dadce2cbc
Olexandr Proskura <olexandr.proskura@mc-dp-test.kyiv.ciklum.net>
2016-02-09 18:55:36 +0200

    Create empty project

---
+++ CustomSwitcher/Views/RappSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CustomSwitcher.Views
+{
+    public class RappSwitcher : View
+    {
+        public RappSwitcher()
+        {
+            this.HeightRequest = 200;
+            this.WidthRequest = 400;
+            this.BackgroundColor = Color.Blue;
+        }
+    }
+}
+
