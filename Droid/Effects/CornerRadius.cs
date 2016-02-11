66b2a559aef30d937f45e34f9a22c1a995fcd560
antixaker <proskura-s@mail.ru>
2016-02-11 16:07:30 +0200

    Improve drawing switcher for Droid

--- Droid/Effects/CornerRadius.cs
+++ Droid/Effects/CornerRadius.cs
@@ -41,29 +41,20 @@ namespace CustomSwitcher.Droid.Effects
 
             var density = Resources.System.DisplayMetrics.Density;
 
-            using (var imageBitmap = Bitmap.CreateBitmap((int)elem.Width, (int)elem.Height, Bitmap.Config.Argb8888))
+            using (var imageBitmap = Bitmap.CreateBitmap((int)(elem.Width * density), (int)(elem.Height * density), Bitmap.Config.Argb8888))
             using (var canvas = new Canvas(imageBitmap))
             using (var paint = new Paint() { Dither = false, Color = elem.BackgroundColor.ToAndroid(), AntiAlias = true })
             {
-                paint.AntiAlias = true;
-
                 paint.Hinting = PaintHinting.On;
-                paint.FilterBitmap = true;
-
-                paint.Flags = PaintFlags.AntiAlias | PaintFlags.FilterBitmap;
+                paint.Flags = PaintFlags.AntiAlias;
 
                 var height = (float)elem.Height;
-//                var df = canvas.IsHardwareAccelerated;
-                canvas.DrawRoundRect(new RectF(0, 0, (float)elem.Width, height), height / 2, height / 2, paint);
 
+                canvas.DrawRoundRect(new RectF(0, 0, (float)elem.Width * density, height * density), height * density / 2, height * density / 2, paint);
+                canvas.Density = (int)density;
+               
                 Container.Background = new BitmapDrawable(imageBitmap);
             }
-        }
-
-        int ConvertPixelsToDp(float pixelValue)
-        { 
-            var dp = (int)((pixelValue) / Forms.Context.Resources.DisplayMetrics.Density);
-            return dp;
         }
     }
 }
